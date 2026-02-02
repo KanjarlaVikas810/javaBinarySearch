cc27dbd1ef7daf8e80e8fd4a91fd11ffd7de3159
KanjarlaVikas810 <vikaskanjarla810@gmail.com>
2026-02-02 13:54:13 +0530

    Update libsort.java

--- libsort.java
+++ libsort.java
@@ -9,3 +9,29 @@ public class LibrarySort {
         System.out.println(Arrays.toString(arr));
     }
 }
+public class BinarySearchExample {
+
+    // Iterative Binary Search
+    public static int binarySearchIterative(int[] arr, int target) {
+        target) {
+            return mid;
+        }
+        if (arr[mid] < target) {
+            return binarySearchRecursive(arr, mid + 1, right, target);
+        } else {
+            return binarySearchRecursive(arr, left, mid - 1, target);
+        }
+    }
+
+    // Main method to test both approaches
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
+        int target = 23;
+
+        int iterativeResult = binarySearchIterative(arr, target);
+        int recursiveResult = binarySearchRecursive(arr, 0, arr.length - 1, target);
+
+        System.out.println("Iterative Search Result: " + iterativeResult);
+        System.out.println("Recursive Search Result: " + recursiveResult);
+    }
+}
