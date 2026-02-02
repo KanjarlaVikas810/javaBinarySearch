7f2d82399392a8f7ce69eedb189c31a382522711
KanjarlaVikas810 <vikaskanjarla810@gmail.com>
2026-02-02 13:28:05 +0530

    Create libsort.java

---
+++ libsort.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class LibrarySort {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 9, 1, 7};
+
+        Arrays.sort(arr);   // Java built‑in sort (Dual-Pivot QuickSort / TimSort)
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
