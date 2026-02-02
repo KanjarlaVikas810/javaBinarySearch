f2d6fb226327cc68fa65227995fc18e2b3e47549
KanjarlaVikas810 <vikaskanjarla810@gmail.com>
2026-02-02 13:39:16 +0530

    Create ON.java

---
+++ ON.java
@@ -0,0 +1,8 @@
+public class NestedLoopExample {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 3; i++) {            // outer loop
+            System.out.println();                 // new line after each row
+        }
+    }
+}
