f7a0c53683b14089fdd8b3a0ffa194082c80bfe7
KanjarlaVikas810 <vikaskanjarla810@gmail.com>
2026-02-02 13:22:37 +0530

    Create nestedloop.java

---
+++ nestedloop.java
@@ -0,0 +1,11 @@
+public class NestedLoopExample {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 3; i++) {            // outer loop
+            for (int j = 1; j <= 4; j++) {        // inner loop
+                System.out.print(j + " ");
+            }
+            System.out.println();                 // new line after each row
+        }
+    }
+}
